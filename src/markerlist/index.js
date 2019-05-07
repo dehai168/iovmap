242f07ef2198a4acf52764ea31fc81bdccdc8b98
dehai168 <dehai168@163.com>
2019-05-07 23:49:23 +0800

    update

--- src/markerlist/index.js
+++ src/markerlist/index.js
@@ -18,14 +18,21 @@
 import offline from "../assets/0.png";
 import online from "../assets/1.png";
 import alarm from "../assets/2.png";
+import m0 from "../assets/m0.png";
+import m1 from "../assets/m1.png";
+import m2 from "../assets/m2.png";
+import m3 from "../assets/m3.png";
+import m4 from "../assets/m4.png";
 
 export class MarkerList {
     constructor(map, canvas, cb) {
         this.map = map;
         this.canvas = canvas;
         this.clickCB = cb;
-        this.imgList = [];
-        this.boundsList = [];
+        this.list = [];  //数据列表
+        this.imgList = []; //车辆图像列表
+        this.boundsList = []; //车辆区域列表
+        this.drawList = []; //绘制列表
         this.popup = null;
         this.popupId = -1;
         this.ctx = canvas.getContext('2d');
@@ -160,26 +167,129 @@ export class MarkerList {
         this._clear();
         if (this.list && this.list.length > 0) {
             console.time('time');
+            this._cluster();
             this._draw();
             console.timeEnd('time');
+        }
+    }
+    /**
+     * 计算集群
+     */
+    _cluster() {
+        const basicSize = 34;
+        const maxZoom = 20;
+        const zoom = this.map.getZoom();
+        const size = this.map.getSize();
+        const viewBounds = L.bounds([0, 0], [size.x, size.y]);
+        const boxSize = Math.floor((maxZoom - zoom) / 2) * basicSize;
+        const xCount = Math.ceil(size.x / boxSize);
+        const yCount = Math.ceil(size.y / boxSize);
+        const boxList = [];
+        for (let i = 0; i < xCount; i++) {
+            for (let j = 0; j < yCount; j++) {
+                const start = [i * boxSize, j * boxSize];
+                const end = [(i + 1) * boxSize, (j + 1) * boxSize];
+                const bounds = L.bounds(start, end);
+                boxList.push({
+                    bounds,
+                    list: [],
+                    one: null,
+                });
+            }
+        }
+        this.list.forEach(element => {
+            let point = this.map.latLngToLayerPoint([element.lat, element.lng]);
+            point = this.map.layerPointToContainerPoint(point); //地理坐标点转换到容器像素点
+            if (viewBounds.contains(point)) { //视界内的点进行绘制
+                boxList.forEach(item => {
+                    if (item.bounds.contains(point)) { //点在box内
+                        item.list.push(point);
+                        item.one = element;
+                    }
+                });
+            }
+        });
+        this.drawList.length = 0;
+        boxList.forEach(item => {
+            const size = item.list.length;
+            const center = this._getPolygonAreaCenter(item.list);
+            this.drawList.push({
+                size,
+                center,
+                one: item.one,
+            });
+        });
+    }
+    /**
+     * 获取多边形质心
+     * @param {*} points 
+     */
+    _getPolygonAreaCenter(points) {
+        if (points.length === 1) {
+            return points[0];
+        } else if (points.length === 2) {
+            return [(points[0].x + points[1].x) / 2, (points[0].y + points[1].y) / 2];
+        } else {
+            //以下为网上down的一个多边形质心算法
+            function Point(x, y) {
+                this.x = x;
+                this.y = y;
+            }
+            function Region(points) {
+                this.points = points || [];
+                this.length = points.length;
+            }
+            Region.prototype.area = function () {
+                var area = 0,
+                    i,
+                    j,
+                    point1,
+                    point2;
+                for (i = 0, j = this.length - 1; i < this.length; j = i, i++) {
+                    point1 = this.points[i];
+                    point2 = this.points[j];
+                    area += point1.x * point2.y;
+                    area -= point1.y * point2.x;
+                }
+                area /= 2;
+                return area;
+            };
+            Region.prototype.centroid = function () {
+                var x = 0,
+                    y = 0,
+                    i,
+                    j,
+                    f,
+                    point1,
+                    point2;
+                for (i = 0, j = this.length - 1; i < this.length; j = i, i++) {
+                    point1 = this.points[i];
+                    point2 = this.points[j];
+                    f = point1.x * point2.y - point2.x * point1.y;
+                    x += (point1.x + point2.x) * f;
+                    y += (point1.y + point2.y) * f;
+                }
+                f = this.area() * 6;
+                return new Point(x / f, y / f);
+            };
+            const region = new Region(points);
+            return region.centroid();
         }
     }
     /**
      * 绘制
      */
     _draw() {
-        let that = this;
-        let size = this.map.getSize();
-        let viewBounds = L.bounds([0, 0], [size.x, size.y]);
-        this.list.forEach(element => {
-            let direction = parseInt(element.direction ? element.direction : 0); // 方向
-            let img = that.imgList[element.state];
-            let point = that.map.latLngToLayerPoint([element.lat, element.lng]);
-            point = that.map.layerPointToContainerPoint(point); //地理坐标点转换到容器像素点
-            if (viewBounds.contains(point)) { //视界内的点进行绘制
+        this.drawList.forEach(item => {
+            if (item.size === 0) {
+
+            } else if (item.size === 1) {//常规绘制
+                let direction = parseInt(item.one.direction ? item.one.direction : 0); // 方向
+                let img = this.imgList[item.one.state];
+                let point = item.center;
                 //绘制图标
                 if (direction <= 0 || direction >= 360) {
-                    that.ctx.drawImage(img, point.x - img.width / 2, point.y - img.height / 2);
+                    this.ctx.drawImage(img, point.x - img.width / 2, point.y - img.height / 2);
                 } else {
                     let angle = 0;
                     if (direction > 0 && direction <= 180) {
@@ -187,30 +297,52 @@ export class MarkerList {
                     } else {
                         angle = Math.PI * (direction / 180 - 2);
                     }
-                    that.ctx.rotate(angle);
-                    that.ctx.drawImage(img, point.x - img.width / 2, point.y - img.height / 2);
-                    that.ctx.rotate(-angle);
+                    this.ctx.rotate(angle);
+                    this.ctx.drawImage(img, point.x - img.width / 2, point.y - img.height / 2);
+                    this.ctx.rotate(-angle);
                 }
                 //绘制文字
-                let width = element.text.length * 5;
+                let width = item.one.text.length * 5;
                 let height = 10;
                 let x = point.x - width / 2;
                 let y = point.y - img.height / 2 - height - 2;
                 //框部分
-                that.ctx.fillStyle = '#FFFFFF';
-                that.ctx.fillRect(x, y, width, height);
+                this.ctx.fillStyle = '#FFFFFF';
+                this.ctx.fillRect(x, y, width, height);
                 //字体部分
-                that.ctx.font = "7px";
-                that.ctx.strokeStyle = '#000000';
-                that.ctx.strokeText(element.text, x, y + height, width);
+                this.ctx.font = "7px";
+                this.ctx.strokeStyle = '#000000';
+                this.ctx.strokeText(item.one.text, x, y + height, width);
                 //范围列表
                 let bounds = L.bounds([[x, y], [point.x + img.width / 2, point.y + img.height / 2]]);
-                that.boundsList.push({
-                    ele: element,
+                this.boundsList.push({
+                    ele: item.one,
                     bounds: bounds,
                 });
                 //气泡
-                that._setPopupLatLng(element);
+                this._setPopupLatLng(item.one);
+            } else {//集群绘制
+                const img = new Image();
+                const point = item.center;
+                const that = this;
+                img.onload = function () {
+                    const textWidth = 25;
+                    that.ctx.drawImage(img, point.x - img.width / 2, point.y - img.height / 2);
+                    that.ctx.font = "7px";
+                    that.ctx.strokeStyle = '#000000';
+                    that.ctx.strokeText(item.size, point.x - textWidth / 2, point.y + 4, textWidth);
+                };
+                if (item.size <= 1000) {
+                    img.src = m0;
+                } else if (item.size <= 2000) {
+                    img.src = m1;
+                } else if (item.size <= 4000) {
+                    img.src = m2;
+                } else if (item.size <= 8000) {
+                    img.src = m3;
+                } else {
+                    img.src = m4;
+                }
             }
         });
     }
